10a3624ccdfe0aebab0614b62506459424556946
Dhruv Patel <dhruv.patel1@yahoo.com>
2020-06-06 18:37:04 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
@@ -12,6 +11,6 @@ app.get("*", (req, res) =>{
     res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port, () =>{
+app.listen(process.env.PORT, () =>{
     console.log("Server is up");
 });
